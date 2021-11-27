ac94e9b1a365a3cf5dbd8fa2ec14f5a59752849b
Kazuhito Osabe <kzosabe@wip.place>
2021-11-27 10:14:54 +0900

    Prepare v0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -5,9 +5,9 @@ sys.path.insert(0, os.path.abspath("../switchbot_client"))
 project = "switchbot-client"
 copyright = "2021, kzosabe"
 author = "kzosabe"
-release = "0.2.0"
+release = "0.3.0"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", ".DS_Store"]
 html_theme = "alabaster"
-html_static_path = ["_static"]
+html_static_path = []
